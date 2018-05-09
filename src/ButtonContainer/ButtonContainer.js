b520259ff865003ec0f2200ab8c8a99efd41e72a
Ecksi <jimmy.dechant@gmail.com>
2018-05-09 06:31:00 -0600

    Add button container boiler plate

---
+++ src/ButtonContainer/ButtonContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ButtonContainer = () => {
+
+  return (
+    <div>
+      <button>People</button>
+      <button>Planets</button>
+      <button>Vehicles</button>
+    </div>
+  );
+};
+
+export default ButtonContainer;
