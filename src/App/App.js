70e112e4c88dad0ad6975406cbfc9e601d2ff2e0
Ecksi <jimmy.dechant@gmail.com>
2018-05-11 22:54:32 -0600

    Pass down props to components and import proper files

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import ButtonContainer from '../ButtonContainer/ButtonContainer';
+import CardContainer from '../CardContainer/CardContainer';
 import TextCrawl from '../TextCrawl/TextCrawl';
+import { fetchPeople, fetchPlanets, fetchVehicles } from '../ApiCalls/ApiCalls';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ButtonContainer />
+        <button onClick={this.fetchData}>People</button>
+        <button onClick={this.fetchData}>Planets</button>
+        <button onClick={this.fetchData}>Vehicles</button>
+        <button>Favorites</button>
+        <CardContainer 
+          people={this.state.people}
+          planets={this.state.planets}
+          vehicles={this.state.vehicles}
+        />
         <TextCrawl />
       </div>
     );
