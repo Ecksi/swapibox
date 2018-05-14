8b6dc072f908ca4260abcb2b0c7889a1da044f83
Ecksi <jimmy.dechant@gmail.com>
2018-05-13 22:09:56 -0600

    Refactor and add proptypes to Favorites.js

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Favorites.css';
 
 const Favorites = (props) => {
   const addToFavorites = () => {
-    if (props.favorites) {
-      if (!props.favorites.find(favorite => favorite.name === props.card.name)) {
-        props.addFavorite(props.card);
-      } else {
-        props.removeFavorite(props.card);
-      }
-    }
+    const { card, favorites } = props;
+
+    favorites.find(favorite => favorite.name === card.name) ?
+      props.removeFavorite(card) :
+      props.addFavorite(card);
   };
 
   return (
     <div>
       <button
         onClick={addToFavorites}
+        className="favoriteButton"
       >
-      Add to Favorites
       </button>
     </div>
   );
+};
+
+Favorites.propTypes = {
+  card: PropTypes.object.isRequired,
+  favorites: PropTypes.array.isRequired,
+  addFavorite: PropTypes.func.isRequired,
+  removeFavorite: PropTypes.func.isRequired
 };
 
 export default Favorites;
